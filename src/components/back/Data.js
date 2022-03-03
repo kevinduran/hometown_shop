30b38d59056a96cf02e52e314018ad88a62db3ef
Kevin <kevinalexanderduran8@gmail.com>
2022-03-03 13:34:06 -0800

    test

---
+++ src/components/back/Data.js
@@ -0,0 +1,59 @@
+//we begin by making an array filled with product data
+
+const data = [
+    {
+        id:"1",
+        name: "ball",
+        price: 24,
+        image: ""
+    },
+    {
+        id:"3",
+        name: "cleats",
+        price: 79,
+        image: ""
+    },
+    {
+        id:"4",
+        name: "jersey",
+        price: 89,
+        image: ""
+    },
+    {
+        id:"5",
+        name: "nets",
+        price: 200,
+        image: ""
+    },
+    {
+        id:"6",
+        name: "cones",
+        price: 12,
+        image: ""
+    },
+    {
+        id:"7",
+        name: "tape",
+        price: 8,
+        image: ""
+    },
+    {
+        id:"8",
+        name: "gear bag",
+        price: 35,
+        image: ""
+    },
+    {
+        id:"9",
+        name: "",
+        price: 89,
+        image: ""
+    },
+    {
+        id:"10",
+        name: "jersey",
+        price: 89,
+        image: ""
+    }
+]
+
