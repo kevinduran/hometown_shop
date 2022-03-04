9c2f1813ee34559df851d9e4ed47e6f0f5d79165
Kevin <kevinalexanderduran8@gmail.com>
2022-03-04 00:44:59 -0800

    test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import data from './components/back/Data/Data';
+import Header from './components/front/Header/Header'
+import {BrowserRouter as Router} from 'react-router-dom'
+
 
 function App() {
+
+  const {productItems} = data;
+
   return (
     <div className="App">
-      
+      <Router>
+        <Header/>
+      </Router>
     </div>
   );
 }
---
+++ src/components/front/Header/Header.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './Header.css'
+
+export default function Header() {
+    return (
+        <header className="header">
+            <div>
+                <h1>
+                    <Link to='/' className='logo'>
+                        Hometown Barbershop Apparel
+                    </Link>
+                </h1>
+            </div>
+            <div className="header-links">
+                <ul>
+                    <li>
+                        <Link to='/'>Home</Link>
+                    </li>
+                    <li>
+                        <Link to='/signup'>Sign-Up</Link>
+                    </li>
+                    <li>
+                        <Link to='/cart'>Cart</Link>
+                    </li>
+                </ul>
+            </div>
+        </header>
+    )
+}
