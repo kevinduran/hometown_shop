2999fb7c6f52dfd2fa78f045cd9617f01e103eb7
Kevin <kevinalexanderduran8@gmail.com>
2022-03-05 05:46:18 -0800

    test

--- src/components/front/Routes/Routez.js
+++ src/components/front/Routes/Routez.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Products from '../Products/Products'
 import {Route, Routes} from 'react-router-dom'
+import Signup from '../Signup/Signup'
+
 
 
 export default function Routez({productItems}) {
@@ -8,6 +10,8 @@ export default function Routez({productItems}) {
         <div>
             <Routes>
                 <Route path='/' exact  element={<Products productItems={productItems} />}/>
+                <Route path='/signup' exact  element={<Signup  />}/>
+
             </Routes>
         </div>
     )
