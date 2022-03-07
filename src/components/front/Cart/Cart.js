4a1e1f77bdc0856606294e9f00123f05487ffa2a
Kevin <kevinalexanderduran8@gmail.com>
2022-03-07 05:08:50 -0800

    test

--- src/components/front/Cart/Cart.js
+++ src/components/front/Cart/Cart.js
@@ -1,6 +1,9 @@
 import React from 'react'
 
 function Cart({cartItems,handleAddProduct,handleRemoveProduct}) {
+    
+    
+        const totalPrice = cartItems.reduce((price,item)=>price + item.quantity * item.price, 0)
     return (
         <div className="cart-items">
             <div className="cart-items-header">Cart Items</div>
@@ -8,28 +11,38 @@ function Cart({cartItems,handleAddProduct,handleRemoveProduct}) {
             {cartItems.length === 0 && (
                 <div className="cart-items-empty">no items are added</div>
             )}
+            <div>
+                {cartItems.map((item)=>(
+                    <div key="item.id" className="cart-item-list">
+                        <img 
+                            alt={item.name} 
+                            src={item.image} 
+                            className="cart-items-image" 
+                        />
+                        <div className="cart-items-name">
+                            {item.name}
+                        </div>
+                        <div className="cart-items-functions">
+                            <button className="cart-items-add" onClick={()=> handleAddProduct(item)}>+</button>
+                            <button className="cart-items-remove" onClick={()=> handleRemoveProduct(item)}>-</button>
+                        </div>
+                        <div className="cart-items-price">
+                            {item.quantity} * ${item.price}
+                        </div>
 
-            {cartItems.map((item)=>(
-                <div key="item.id" className="cart-item-list">
-                    <img 
-                        alt={item.name} 
-                        src={item.image} 
-                        className="cart-items-image" 
-                    />
-                    <div className="cart-items-name">
-                        {item.name}
                     </div>
-                    <div className="cart-items-functions">
-                        <button className="cart-items-add" onClick={()=> handleAddProduct(item)}>+</button>
-                        <button className="cart-items-remove" onClick={()=> handleRemoveProduct(item)}>-</button>
-                    </div>
-                    <div className="cart-items-price">
-                        {item.quantity} * ${item.price}
-                    </div>
+                ))}
+            </div>    
 
+            <div className="cart-items-total-price-name">
+                Total Price...
+                <div className="cart-item-total-price">
+                    ${totalPrice}
                 </div>
-            ))}
-        </div>    
+            </div>
+        </div>  
+        
+        
     )
 }
 
