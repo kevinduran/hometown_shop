f5512697d62473f0fbaf03dc59b30fde50d18dee
Kevin <kevinalexanderduran8@gmail.com>
2022-03-12 05:13:39 -0800

    CHECKPOINT

--- src/components/back/Data/Data.js
+++ src/components/back/Data/Data.js
@@ -4,55 +4,55 @@ const data = {
         {
             id:"1",
             name: "hand of god",
-            price: 22,
+            price: 21.00,
             image: "./assets/clothes1Small.png"
         },
         {
             id:"2",
             name: "sudden death",
-            price: 55,
+            price: 54.99,
             image: "./assets/clothes2Small.png"
         },
         {
             id:"3",
             name: "views",
-            price: 30,
+            price: 29.99,
             image: "./assets/clothes3Small.png"
         },
         {
             id:"4",
             name: "vanilla",
-            price: 22,
+            price: 21.00,
             image: "./assets/clothes4Small.png"
         },
         {
             id:"5",
             name: "sad girl hours",
-            price: 27,
+            price: 27.00,
             image: "./assets/clothes5Small.png"
         },
         {
             id:"6",
             name: "life is strange",
-            price: 30,
+            price: 29.99,
             image: "./assets/clothes6Small.png"
         },
         {
             id:"7",
             name: "sum of its parts",
-            price: 22,
+            price: 21.00,
             image: "./assets/clothes7Small.png"
         },
         {
             id:"8",
             name: "court of owls",
-            price: 20,
+            price: 21.00,
             image: "./assets/clothes8Small.png"
         },
         {
             id:"9",
             name: "kakarot",
-            price: 35,
+            price: 34.99,
             image: "./assets/clothes9Small.png"
         }
     ]
