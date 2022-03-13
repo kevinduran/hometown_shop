db8fba022b7317b7bf80b23b5cf9357f89974a22
Kevin <kevinalexanderduran8@gmail.com>
2022-03-12 16:51:23 -0800

    test

--- src/components/back/Data/Data.js
+++ src/components/back/Data/Data.js
@@ -4,7 +4,7 @@ const data = {
         {
             id:"1",
             name: "hand of god",
-            price: 21.00,
+            price: 21.99,
             image: "./assets/clothes1Small.png"
         },
         {
@@ -22,13 +22,13 @@ const data = {
         {
             id:"4",
             name: "vanilla",
-            price: 21.00,
+            price: 21.99,
             image: "./assets/clothes4Small.png"
         },
         {
             id:"5",
             name: "sad girl hours",
-            price: 27.00,
+            price: 27.99,
             image: "./assets/clothes5Small.png"
         },
         {
@@ -40,13 +40,13 @@ const data = {
         {
             id:"7",
             name: "sum of its parts",
-            price: 21.00,
+            price: 21.99,
             image: "./assets/clothes7Small.png"
         },
         {
             id:"8",
             name: "court of owls",
-            price: 21.00,
+            price: 21.99,
             image: "./assets/clothes8Small.png"
         },
         {
