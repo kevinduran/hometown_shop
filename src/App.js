4c3ba7026a3fa8e373b01f2eea51f18086b8fe34
Kevin <kevinalexanderduran8@gmail.com>
2022-09-09 20:51:14 -0700

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './assets/logo.png';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='coming_soon'>
+        <h1>Coming soon...</h1>
+        <div>
+        <img className='coming_soon_logo' alt="logo" src={logo} /> 
+          <div>
+            <h3>Apparrel Shop</h3>
+            <h6>2.0</h6>
+          </div>
+        </div>
+        <a href='https://hometownbarbershop805.com/'><button>go back</button></a>
+      </div>
+      
     </div>
   );
 }
