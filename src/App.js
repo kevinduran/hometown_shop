03bcba30ee49e7e1ae697871728f769ca1533cde
Kevin <kevinalexanderduran8@gmail.com>
2022-09-10 04:06:38 -0700

    progress

--- src/App.js
+++ src/App.js
@@ -7,16 +7,13 @@ function App() {
     <div className="App">
       <div className='coming_soon'>
         <h1>Coming soon...</h1>
-        <div>
-        <img className='coming_soon_logo' alt="logo" src={logo} /> 
-          <div>
-            <h3>Apparrel Shop</h3>
-            <h6>2.0</h6>
-          </div>
+        <div className='coming_soon-small-container'>
+          <img className='coming_soon_logo' alt="logo" src={logo} /> 
+          <h3>Apparrel Shop</h3>
+          <h6>2.0</h6>
         </div>
-        <a href='https://hometownbarbershop805.com/'><button className='coming_soon_button'>go back</button></a>
+        <a href='https://hometownbarbershop805.com/'><button className='coming_soon_button'>go back</button></a>       
       </div>
-      
     </div>
   );
 }
